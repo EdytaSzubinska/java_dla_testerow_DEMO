f502c53c09c8fc3a88de3ef2028003eaa4330b61
Edyta Szubinska <e.szubinska@gmail.com>
2021-01-06 14:21:48 +0100

    added homework_1

---
+++ src/zadanie_domowe_1.java
@@ -0,0 +1,19 @@
+public class zadanie_domowe_1 {
+    public zadanie_domowe_1() {
+    }
+
+    public static void main(String[] args) {
+        int numOne = 5;
+        int numTwo = 4;
+        int numThree = 10;
+        int numFour = 1;
+        int sum1 = numOne + numTwo;
+        int sum2 = numThree + numFour;
+        System.out.println(sum1);
+        System.out.println(sum2);
+        if (sum1 % sum2 == 0) {
+        }
+
+        System.out.println("Nie wszystkie liczby są parzyste");
+    }
+}
