49f9c0c14576951e840ee22c13549ffd788258bd
Edyta Szubinska <e.szubinska@gmail.com>
2021-01-09 17:29:43 +0100

    zadanie_6

--- src/computer/MainApp.java
+++ src/computer/MainApp.java
@@ -3,12 +3,9 @@ package computer;
 import computer.PC;
 import computer.Laptop;
 
-
 public class MainApp {
 
     public static void main(String[] args) {
-
-        // POLIMORFIZM - jest to wiele form
 
         PC officeComputer1 = new PC("Office computer 1", "HP", 500, 128);
         Computer officeComputer2 = new PC("Office computer 2", "HP", 500, 128);
@@ -25,29 +22,11 @@ public class MainApp {
         officeComputer1.showComputerName();
         ((PC) officeComputer2).showComputerName();
 
-        // ABSTRAKCJA vs. KONKRETNE ROZWIĄZANIA
-
-        // z klasy abstrakcyjnej mie można utworzyć konkretnego obietu, ale można używać tego typu do referencji
-        // jeżeli klasa posiada przynajmniej jedną metodę abstrakcyjną, to cała klaa też musi bć abstakcyjna
-        // klasy dziedziczące muszą(!!!) napisać wszystkie metody abstrakcyjne z abstrakcyjnej klasy matki
-
         Computer officeComputer = new PC("Office computer 3", "HP", 500, 128);
         Computer gamingLaptop = new Laptop("XGames", "HM GAMES", 500, 256, 50);
 
         System.out.println(officeComputer.volumeUp());
         System.out.println(gamingLaptop.volumeUp());
-
-        System.out.println(officeComputer.volumeUp());
-        System.out.println(officeComputer.volumeUp());
-        System.out.println(officeComputer.volumeUp());
-
-        System.out.println(gamingLaptop.volumeDown());
-        System.out.println(gamingLaptop.volumeDown());
-        System.out.println(gamingLaptop.volumeDown());
-        System.out.println(gamingLaptop.volumeDown());
-        System.out.println(gamingLaptop.volumeDown());
-        System.out.println(gamingLaptop.volumeDown());
-
 
         officeComputer.volumeUp();
         System.out.println(officeComputer.getVolumeLevel());
